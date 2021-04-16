2acd90ce262dd1d4a1db009477854538cd0e9d10
fenyrmaster <brandony2004@gmail.com>
2021-04-16 17:55:55 -0500

    just a message

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views",path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")))
-
+// wow
 app.use(
   helmet({
     contentSecurityPolicy: {
